f7c3d1938bbb769e7fde957b382b5591a999e308
Sławomir Zaraziński <jestemKoderem@gmail.com>
2021-03-31 21:26:44 +0200

    Return empty response

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, status
+from fastapi.responses import Response
+
 from machine_control.motors.CameraConveyorController import CameraConveyorController
 
 app = FastAPI()
@@ -8,8 +10,10 @@ conveyor_controller = CameraConveyorController()
 @app.get("/start", status_code=status.HTTP_204_NO_CONTENT)
 async def start(frequency: int = 200, duty_cycle: int = 50):
     conveyor_controller.run(hz=frequency, duty_cycle=duty_cycle)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @app.get("/stop", status_code=status.HTTP_204_NO_CONTENT)
 async def stop():
     conveyor_controller.stop()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
