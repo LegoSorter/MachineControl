f68316d8b8f935818b200833cba8fcbffed1e10b
Sławomir Zaraziński <jestemKoderem@gmail.com>
2021-04-10 01:45:41 +0200

    Add SplittingConveyorController

---
+++ machine_control/motors/SplittingConveyorController.py
@@ -0,0 +1,55 @@
+import RPi.GPIO as gpio
+
+
+class SplittingConveyorController:
+    DEFAULT_FREQUENCY = 4
+
+    INPUT_1 = 23  # Input 1 for spinning direction
+    INPUT_2 = 24  # Input 2 for spinning direction
+    ENA = 25  # This is for controlling speed using Pulse Width Modulation
+
+    def __init__(self):
+        self.initialized = False
+        self.pwm = None
+
+    """
+    IN1 IN2 Spinning direction
+     F   F      MOTOR OFF
+     T   F       FORWARD
+     F   T       BACKWARD
+     F   F      MOTOR OFF
+    """
+
+    def setup(self):
+        gpio.setmode(gpio.BCM)
+        gpio.setup(self.INPUT_1, gpio.OUT)
+        gpio.setup(self.INPUT_2, gpio.OUT)
+        gpio.setup(self.ENA, gpio.OUT)
+
+        self.pwm = gpio.PWM(self.ENA, self.DEFAULT_FREQUENCY)
+        self.initialized = True
+
+    def run(self, forward: bool = True, hz: int = DEFAULT_FREQUENCY, duty_cycle: int = 60):
+        if self.initialized is False:
+            self.setup()
+
+        self.pwm.ChangeFrequency(hz)
+        self.pwm.start(duty_cycle)
+
+        if forward:
+            gpio.output(self.INPUT_1, True)
+            gpio.output(self.INPUT_2, False)
+        else:
+            gpio.output(self.INPUT_1, False)
+            gpio.output(self.INPUT_2, True)
+
+    # def run_with_speed(self, hz: int = 100, duty_cycle: int = 50):
+    #     pwm_value = gpio.PWM(self.ENA, hz)
+    #     pwm_value.start(duty_cycle)
+
+    def stop(self):
+        if self.pwm is not None:
+            self.pwm.stop()
+
+        gpio.cleanup()
+        self.initialized = False
--- main.py
+++ main.py
@@ -2,18 +2,22 @@ from fastapi import FastAPI, status
 from fastapi.responses import Response
 
 from machine_control.motors.CameraConveyorController import CameraConveyorController
+from machine_control.motors.SplittingConveyorController import SplittingConveyorController
 
 app = FastAPI()
-conveyor_controller = CameraConveyorController()
+camera_conveyor_controller = CameraConveyorController()
+splitting_conveyor_controller = SplittingConveyorController()
 
 
 @app.get("/start", status_code=status.HTTP_204_NO_CONTENT)
 async def start(frequency: int = 200, duty_cycle: int = 50):
-    conveyor_controller.run(hz=frequency, duty_cycle=duty_cycle)
+    camera_conveyor_controller.run(hz=frequency, duty_cycle=duty_cycle)
+    splitting_conveyor_controller.run()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @app.get("/stop", status_code=status.HTTP_204_NO_CONTENT)
 async def stop():
-    conveyor_controller.stop()
+    camera_conveyor_controller.stop()
+    splitting_conveyor_controller.stop()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
