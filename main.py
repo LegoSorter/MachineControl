b042fd0706cb75ec37d3604b030110e30dd0fea3
uiopak <44174807+uiopak@users.noreply.github.com>
2023-01-11 22:43:13 +0100

    Add more options to start/stop conveyors

--- main.py
+++ main.py
@@ -21,3 +21,27 @@ async def stop():
     camera_conveyor_controller.stop()
     splitting_conveyor_controller.stop()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.get("/start_camera_conveyor", status_code=status.HTTP_204_NO_CONTENT)
+async def start_camera_conveyor(frequency: int = 15, duty_cycle: int = 50):
+    camera_conveyor_controller.run(hz=frequency, duty_cycle=duty_cycle)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.get("/stop_camera_conveyor", status_code=status.HTTP_204_NO_CONTENT)
+async def stop_camera_conveyor():
+    camera_conveyor_controller.stop()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.get("/start_splitting_conveyor", status_code=status.HTTP_204_NO_CONTENT)
+async def start_splitting_conveyor(frequency: int = 20, duty_cycle: int = 40):
+    splitting_conveyor_controller.run(hz=frequency, duty_cycle=duty_cycle)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.get("/stop_splitting_conveyor", status_code=status.HTTP_204_NO_CONTENT)
+async def stop_splitting_conveyor():
+    splitting_conveyor_controller.stop()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
